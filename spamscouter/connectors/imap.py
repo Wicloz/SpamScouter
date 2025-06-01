425069793ff05deaca6d70c66252fc668af59990
Wilco de Boer <deboer.wilco@gmail.com>
2025-06-02 00:32:32 +0200

    Rewrite the IMAP connector to use `imap-tools`

--- spamscouter/connectors/imap.py
+++ spamscouter/connectors/imap.py
@@ -1,33 +1,49 @@
 from ..connectors._base import ConnectorBase
 from ..message import message_builder
+from contextlib import contextmanager
+from imap_tools import MailBox
+import re
 
 
 class ConnectorIMAP(ConnectorBase):
     def recipients(self):
-        yield from self.config.recipients
+        yield from self.config.imap_recipients
+
+    @contextmanager
+    def _mailbox(self, recipient):
+        with MailBox(
+            self.config.imap_host, self.config.imap_port,
+        ).login(
+            self.config.imap_get_user(recipient), self.config.imap_get_pass(recipient),
+        ) as mailbox:
+            yield mailbox
+
+    def _list_all_folders(self, mailbox):
+        for folder in mailbox.folder.list():
+            yield folder.name, {flag.lstrip('\\').lower() for flag in folder.flags}
 
     def iterate_messages_for_user(self, recipient):
-        with self.config.server(recipient) as server:
+        with self._mailbox(recipient) as mailbox:
 
-            for folder in server.list_folders():
-                if b'\\Sent' in folder[0] or b'\\Drafts' in folder[0] or b'\\Trash' in folder[0]:
+            for folder, flags in self._list_all_folders(mailbox):
+                if 'sent' in flags or 'drafts' in flags or 'trash' in flags:
                     continue
-                server.select_folder(folder[2], readonly=True)
-                flags = {flag.lstrip(b'\\').decode('ASCII') for flag in folder[0]}
+                mailbox.folder.set(folder, readonly=True)
 
-                messages = server.search()
-                for idx in range(0, len(messages), 100):
-                    for uid, data in server.fetch(messages[idx:idx + 100], [b'BODY[]', b'FLAGS']).items():
-                        yield message_builder(data[b'BODY[]'], b'\\Seen' in data[b'FLAGS'], uid, folder[2], flags, self.config)
+                for response in mailbox._fetch_in_bulk(mailbox.uids(), '(RFC822 FLAGS)', False, 100):
+                    header = response[0][0].decode('ASCII')
+                    uid = int(re.search(r'UID (\d+)', header).group(1))
+                    read = re.search(r'FLAGS \([^\(\)]*\\Seen[^\(\)]*\)', header) is not None
+                    message = response[0][1]
+                    yield message_builder(message, read, uid, folder, flags, self.config)
 
     def estimate_message_count_for_user(self, recipient):
         estimate = 0
-        with self.config.server(recipient) as server:
 
-            for folder in server.list_folders():
-                if b'\\Sent' in folder[0] or b'\\Drafts' in folder[0] or b'\\Trash' in folder[0]:
+        with self._mailbox(recipient) as mailbox:
+            for folder, flags in self._list_all_folders(mailbox):
+                if 'sent' in flags or 'drafts' in flags or 'trash' in flags:
                     continue
-                server.select_folder(folder[2], readonly=True)
+                estimate += mailbox.folder.status(folder, ['MESSAGES'])['MESSAGES']
 
-                estimate += len(server.search())
         return estimate
