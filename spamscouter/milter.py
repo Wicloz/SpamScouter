6e2be931ac3ab0990cf3f26b181dcd2517e5f9f3
Wilco de Boer <deboer.wilco@gmail.com>
2025-06-02 05:06:35 +0200

    Add untested milter implementation

---
+++ spamscouter/milter.py
@@ -0,0 +1,90 @@
+import Milter
+from Milter import decode
+import email
+from tokenizers import BertWordPieceTokenizer
+from gensim.models.doc2vec import Doc2Vec
+import torch
+from argparse import ArgumentParser
+from pathlib import Path
+from .message import message_to_text
+from .models import SpamRegressor
+
+
+class SpamScouterMilter(Milter.Base):
+    def __init__(self):
+        self.recipients = []
+        self.message = b''
+
+    def envrcpt(self, address, *_):
+        # store envelope recipients for loading models
+        self.recipients.append(address)
+
+        # tell the MTA to continue
+        return Milter.CONTINUE
+
+    @decode('bytes')
+    def header(self, key, value):
+        # add header to the reconstituted message
+        self.message += key.encode('ascii')
+        self.message += b': '
+        self.message += value
+        self.message += b'\r\n'
+
+        # tell the MTA to continue
+        return Milter.CONTINUE
+
+    def eoh(self):
+        # add a blank line to the reconstituted message
+        self.message += b'\r\n'
+
+        # tell the MTA to continue
+        return Milter.CONTINUE
+
+    def body(self, chunk):
+        # add body chunk to the reconstituted message
+        self.message += chunk
+
+        # tell the MTA to continue
+        return Milter.CONTINUE
+
+    def eom(self):
+        # convert the message to text
+        text = message_to_text(email.message_from_bytes(self.message))
+        print(text)
+
+        # convert the text to a vector
+        vector = list(VECTORIZER.infer_vector(TOKENIZER.encode(text).tokens))
+        print(vector)
+
+        # predict the global spam probability
+        with torch.no_grad():
+            tensor = torch.tensor(vector, dtype=torch.float32).view(1, -1)
+            spam_probability = REGRESSOR(tensor).item()
+
+        # add the spam probability as a header
+        print('Spam Probability:', spam_probability)
+        self.addheader('X-Spam-Scouter-Probability', str(spam_probability))
+
+        # reset instance variables, more emails could be sent over this connection
+        self.__init__()
+
+        # tell the MTA to continue
+        return Milter.CONTINUE
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('model_store_dir', type=Path)
+    parser.add_argument('--protocol', type=str, default='inet')
+    parser.add_argument('--address', type=str, default='3639@localhost')
+    args = parser.parse_args()
+
+    TOKENIZER = BertWordPieceTokenizer(args.model_store_dir / 'tokenizer.json')
+    VECTORIZER = Doc2Vec.load(args.model_store_dir / 'doc2vec.model')
+
+    REGRESSOR = SpamRegressor()
+    REGRESSOR.load(args.model_store_dir / 'regressor.pt')
+    REGRESSOR.eval()
+
+    Milter.factory = SpamScouterMilter
+    Milter.runmilter('SpamScouterMilter', f'{args.protocol}:{args.address}', 60)
