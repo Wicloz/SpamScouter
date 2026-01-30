4b142aecae1716689a16a5aeb86e6ea2b31bae96
Wilco de Boer <deboer.wilco@gmail.com>
2026-01-31 00:01:02 +0100

    Specify amount of trials using the CLI

--- hpo_experiments.py
+++ hpo_experiments.py
@@ -2,6 +2,7 @@ from smac import MultiFidelityFacade, Scenario
 from spamscouter.trainer import Trainer, CS
 from spamscouter.settings import BaseSettings
 from os import cpu_count
+from sys import argv
 
 
 class ScouterSettings(BaseSettings):
@@ -10,6 +11,8 @@ class ScouterSettings(BaseSettings):
 
 
 if __name__ == '__main__':
+    trials = int(argv[1])
+
     trainer = Trainer(ScouterSettings())
     trainer.initialize_hpo()
 
@@ -18,9 +21,10 @@ if __name__ == '__main__':
         min_budget=trainer.min_budget,
         max_budget=trainer.max_budget,
         deterministic=True,
+        n_trials=trials,
         n_workers=cpu_count() - 1,
         output_directory='.smac/',
     ), trainer.train_and_validate)
-    incumbent = smac.optimize()
 
+    incumbent = smac.optimize()
     print(f'Best HP Configuration: {incumbent}')
