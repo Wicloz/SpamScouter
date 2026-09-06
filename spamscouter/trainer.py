6b7d6f1dc83fe95093698d039b71ae89cbcbd37d
Wilco de Boer <deboer.wilco@gmail.com>
2026-09-06 14:33:26 +0200

    Update CS defaults based on full HPO incumbent

--- spamscouter/trainer.py
+++ spamscouter/trainer.py
@@ -23,11 +23,11 @@ CONNECTORS = {
 
 
 CS = ConfigurationSpace()
-CS.add(Integer('document_vector_size', (100, 1000), default=500))
+CS.add(Integer('document_vector_size', (100, 1000), default=766))
 CS.add(Categorical('message_processing_method', MESSAGE_PROCESS_METHODS.keys(), default='body_unicode'))
-CS.add(Float('vocab_size_per_message', (0, 2), default=1, distribution=Beta(4, 4)))
-CS.add(Integer('vocab_token_min_count', (1, 10000), default=100, log=True))
-CS.add(Integer('max_message_characters', (1000, 500000), default=500000, log=True))
+CS.add(Float('vocab_size_per_message', (0, 2), default=1.1588915949217, distribution=Beta(4, 4)))
+CS.add(Integer('vocab_token_min_count', (1, 10000), default=35, log=True))
+CS.add(Integer('max_message_characters', (1000, 500000), default=6873, log=True))
 
 REGRESSORS = {
     'SVM': svm.SVR,
@@ -46,11 +46,11 @@ HYPER_PARAMETERS = {
         Integer('n_neighbors', (1, 100), default=5),
     ],
     'NeuralNetwork': [
-        Integer('hidden_layer_size', (10, 1000), default=100, log=True),
+        Integer('hidden_layer_size', (10, 1000), default=126, log=True),
         Categorical('final_activation_function', NeuralNetworkRegressor.FINAL_ACTIVATION_FUNCTIONS, default='sigmoid'),
         Float('learning_rate', (1e-5, 1e-1), default=NeuralNetworkRegressor.LEARNING_RATE, log=True),
         Float('weight_decay', (1e-3, 1e1), default=NeuralNetworkRegressor.WEIGHT_DECAY, log=True),
-        Categorical('balance_classes', (True, False), default=True),
+        Categorical('balance_classes', (True, False), default=False),
     ],
 }
 
