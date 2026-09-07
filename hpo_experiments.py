5d20c1cbe56af0bebcca2f42875e386b5b4b2d08
Wilco de Boer <deboer.wilco@gmail.com>
2026-09-07 13:50:52 +0200

    Always evaluate the default config

--- hpo_experiments.py
+++ hpo_experiments.py
@@ -64,6 +64,7 @@ if __name__ == '__main__':
 
     scenario = Scenario(
         configspace=CS,
+        use_default_config=True,
         min_budget=trainer.min_budget,
         max_budget=trainer.max_budget,
         deterministic=True,
