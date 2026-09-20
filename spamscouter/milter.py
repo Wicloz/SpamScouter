66a9edb04e33cce618bd1c8b01e690347d58156b
Wilco de Boer <deboer.wilco@gmail.com>
2026-09-20 04:26:17 +0200

    Overwrite any spoofed headers

--- spamscouter/milter.py
+++ spamscouter/milter.py
@@ -78,7 +78,7 @@ class SpamScouterMilter(Milter.Base):
 
         # add the spam probability as a header
         print('> Spam Probability:', spam_probability)
-        self.addheader('X-SpamScouter-Probability', f'{spam_probability:f}')
+        self.chgheader('X-SpamScouter-Probability', f'{spam_probability:f}')
 
         # reset instance variables, more emails could be sent over this connection
         self.__init__()
